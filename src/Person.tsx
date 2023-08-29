6a6501c548f2720a810f8ffefd8a0384d93fce20
Your Name <roshankhatri212@gmail.com>
2023-08-29 19:34:06 +0545

    typescript1

---
+++ src/Person.tsx
@@ -0,0 +1,29 @@
+interface Props{
+  name:string;
+  email:string;
+  age:number;
+  isMarried:boolean;
+  friends:string[];
+  country:Country;
+}
+export enum Country{
+  Nepal="Nepal",
+  India="India",
+  China="China",
+}
+
+
+export const Person=(props:Props)=>{
+    return(
+      <div>
+      <h1>Name:{props.name}</h1>
+      <h1>Email:{props.email}</h1>
+      <h1>Age:{props.age}</h1>
+      <h1>This person {props.isMarried ? "is" :"is not"} married</h1>
+      {props.friends.map((friend)=>(
+        <h1>{friend}</h1>
+      ))}
+      <h1>Country:{props.country}</h1>
+      </div>
+    )
+      }
